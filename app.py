e4c477bcb2f2b7d271526b797a160b76b9c7afd3
BICHURAJ <bichurj887@gmail.com>
2024-03-01 14:47:15 +0000

    tensor serving

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import requests
 import json
 import numpy as np
 from tensorflow.keras.preprocessing import image
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL= os.getenv('LOCAL_URL')
+SERVER_URL=os.getenv('SERVER_URL')
 
 def imageProcess(url):
     try:
@@ -16,19 +22,16 @@ def imageProcess(url):
         print("Error:", e)
         return None
 
-# Provide the folder path and filename
-filename = r'D:\artificial-intelligence\advanced-deep-learning-computer-vision\code\diabetic-retinopathy\data\processed\16_right.jpeg'
 
-
+filename= r'D:\artificial-intelligence\advanced-deep-learning-computer-vision\code\diabetic-retinopathy\data\raw\16_right.jpeg'
 # the image
 imageArray = imageProcess(filename)
 class_labels = ['Level_0', 'Level_1', 'Level_2', 'Level_4']  
 
 # Make a request to TensorFlow Serving
 if imageArray is not None:
-    url = 'http://localhost:8601/v1/models/retinopathy/versions/1:predict'
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=imageArray, headers=headers)
+    response = requests.post(SERVER_URL, data=imageArray, headers=headers)
     predictions = response.json()['predictions'][0]
     predicted_class_index = np.argmax(predictions)
     print("Predictions:", class_labels[predicted_class_index])
